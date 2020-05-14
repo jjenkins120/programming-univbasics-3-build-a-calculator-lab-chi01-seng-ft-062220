fcfe892e3a5988d6976ab03f2d10abb640f44e3f
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-14 20:53:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  minus = num1 - num2
+  puts minus
+  minus
 end
 
 def division(num1, num2)
-
+  dividend = num1 / num2
+  puts dividend
+  dividend
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
+  product
 end
 
 def modulo(num1, num2)
-
+  modulus = num2 % num1
+  puts modulus
+  modulus
 end
 
 def square_root(num)
-
+  root = sqrt(num)
+  puts root
+  root
 end
